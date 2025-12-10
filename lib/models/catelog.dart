360a454d7303be0f2e77c96f6e38648e0117d9bf
Mahajidalam <ansarimahajidalamn@gmail.com>
2025-12-10 22:20:23 +0530

    day 11 completed.

--- lib/models/catelog.dart
+++ lib/models/catelog.dart
@@ -6,7 +6,24 @@ class Item {
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
-
-  
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
 }
+
+final products = [
+  Item(
+    id: "XYZ",
+    name: "XYZ",
+    desc: "XYZ",
+    price: 999,
+    color: '#33505a',
+    image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmDpdESZq-3Wpj65-s4kbDNMSKFbW5eJUY2Q&s',
+  ),
+];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,14 +11,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //         backgroundColor: Colors.white,
+        // backgroundColor: Colors.white,
         // elevation: 0.0,
         // iconTheme: IconThemeData(color: Colors.black),
         // title: Text('Catalog App', style: TextStyle(color: Colors.black)),
         title: Text('Catalog App'),
       ),
       body: Center(
-        child: Container(child: Text('Welcome to $days days of flutter by $name')),
+        child: Container(
+          child: Text(
+            'Welcome to $days days of flutter by $name' +
+                context.runtimeType.toString(),
+          ),
+        ),
       ),
       drawer: MyDrawer(),
     );
